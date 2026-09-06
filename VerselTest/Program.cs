9be55f21bc1480fd52c927953a104724b5d9659a
edena <djaziz12393@gmail.com>
2026-09-06 22:28:13 +0300

    wip

--- VerselTest/Program.cs
+++ VerselTest/Program.cs
@@ -1,5 +1,5 @@
 var builder = WebApplication.CreateSlimBuilder(args);
-var port = Environment.GetEnvironmentVariable("PORT") ?? "80";
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
 builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
 
 var app = builder.Build();
