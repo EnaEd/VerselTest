ef3953e5fdce0e40626ded0108ba1be776b10b99
edena <djaziz12393@gmail.com>
2026-09-06 22:18:54 +0300

    edit csproj

--- VerselTest/Program.cs
+++ VerselTest/Program.cs
@@ -1,14 +1,8 @@
 var builder = WebApplication.CreateSlimBuilder(args);
-var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+var port = Environment.GetEnvironmentVariable("PORT") ?? "80";
 builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
 
 app.UseHttpsRedirection();
 
